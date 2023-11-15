7fccb20856b0e482c54da0d00620980deed5cc20
muizah08 <luthfiyyamuizah@gmail.com>
2023-11-15 12:07:21 +0700

    pertemuan 9

---
+++ latihan.py
@@ -0,0 +1,84 @@
+# Akses list
+list1 = ["ival", "zah", "alfi", "12", "04"]
+print("Tampilkan element ke-3 :", list1[3]) 
+print("Ambil nilai element 2-4", list1[2:5])
+print("Ambil element terakhir :", list1[0:4])
+
+print( 45*"=", "\n")
+
+# Ubah element
+list2 = [11, 12, 13, 14, 15]
+print(list2)
+list2[4] = "Element 4"
+print(list2)
+
+print()
+
+list3 = ["Siapa", "Kamu", "Ngatur", "Hidup", "Aku"]
+print("Sebelum di ubah :", list3)
+list3[0:6] = ["S", "K", "N", "H", "A"]
+print("Setelah di ubah :", list3, "\n")
+
+# Tambah list element
+#  Index   0(-5),   1(-4),  2(-3),   3(-2),   4(-1) 
+lista  = [1, 2, 3, 4, 5]
+print("ListA\n", lista)
+listb  = [6, 7, 8]
+print("ListB Sebelum di tambahkan :\n", listb, "\n")
+
+lista  = [1 ,2 ,3 ,4 ,5]
+print("ListA\n", lista)
+listb  = [6 ,7 ,8 ]
+listb.insert(2, lista[0:2])  
+print("ListB Sesudah di tambahkan :\n", listb, "\n")
+
+# Mendambahkan list B dengan nilai string
+listb.append("Timey")
+print("ListB :\n", listb,"\n")
+
+# Menambahkan list B dengan 3 Nilai
+listb.extend([10, 12, 14])
+print("Mendambahkan ListB dengan 3 nilai :\n", listb,"\n")
+# Menggabungkan ListA dan ListB 
+listN = lista + listb
+print("Gabungan listA & ListB: \n", listN)
+
+# LIST
+nama = []
+nim = []
+nilaiTugas = []
+nilaiUTS = []
+nilaiUAS = []
+nilaiAkhir = []
+
+print()
+
+# Input
+while True:
+    nama.append(input("Masukan nama : "))
+    nim.append(input("Masukan NIM  : "))
+    Tugas = int(input("Nilai Tugas  : ")); 
+    nilaiTugas.append(Tugas)
+    UTS   = int(input("Nilai UTS    : ")); 
+    nilaiUTS.append(UTS)
+    UAS   = int(input("Nilai UAS    : ")); 
+    nilaiUAS.append(UAS)
+
+    nilaiAkhir.append(Tugas * 30/100 + UTS * 35/100 + UAS * 35/100)
+
+    print()
+    _tanya = input("Tambah data ? [y/t]: ")
+    print()
+    if(_tanya == "t"):
+        break
+
+# Output
+print("+----+------------------------------+--------+--------+-------+-------+---------+")
+print("| {0:^2} | {1:^18} | {2:^9} | {3:^6} | {4:^5} | {5:^5} | {6:^7} |".format("No", "Nama", "NIM", "Tugas", "UTS", "UAS", "Akhir"))
+print("-----+------------------------------+--------+--------+-------+-------+---------+")
+
+no = 0
+for nama, nim, Tugas, UTS, UAS, nilaiAkhir in zip(nama, nim, nilaiTugas, nilaiUTS, nilaiUAS, nilaiAkhir):
+    no += 1    
+    print("| {0:>2} | {1:<18} | {2:>8} | {3:>6} | {4:>5} | {5:>5} | {6:>7} |".format(no, nama, nim, Tugas, UTS, UAS, nilaiAkhir))
+print("+----+------------------------------+--------+--------+-------+-------+---------+")
